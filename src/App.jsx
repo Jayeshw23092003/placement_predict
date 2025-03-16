b177184dae26c1623d9a1e2db84f9e89569b7c71
Trapti121 <traptig04@gmail.com>
2025-03-16 17:56:27 +0530

    Added student details and student list page

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ import UploadResume from "./Components/UploadResume.jsx"
 import Profile from "./Components/Profile.jsx"
 import CompanyList from "./Components/CompanyList.jsx"
 import CompanyDetails from './Components/CompanyDetails.jsx';
-import {Route, Routes, BrowserRouter as Router} from "react-router-dom"
+import {Route, Routes, BrowserRouter as Router} from "react-router-dom";
+import StudentList from "./Components/StudentList.jsx";
+import StudentDetails from "./Components/StudentDetails.jsx";
 import ResponsiveAppBar from "./Components/AppBar.jsx";
 function App() {
   return (
@@ -26,6 +28,8 @@ function App() {
           <Route path = "/companies_dashboard" element = {<CompanyList/>}></Route> 
           <Route path = "/users_dashboard" element = {<CompanyList/>}></Route> 
          <Route path="/company/:id" element={<CompanyDetails />}></Route>
+         <Route path="/students" element={<StudentList />} />
+          <Route path="/students/:id" element={<StudentDetails />} />
          <Route path="/navbar" element={<ResponsiveAppBar />} />
         </Routes>
       </Router>
